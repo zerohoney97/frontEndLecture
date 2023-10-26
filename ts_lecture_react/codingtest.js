a7f9f85ff2381cd54a22701c47e8f49d547b76ca
zerohoney <zerohoney97@gmail.com>
2023-10-26 17:27:18 +0900

    asd

--- ts_lecture_react/codingtest.js
+++ ts_lecture_react/codingtest.js
@@ -1,22 +1,65 @@
 const nodeNum = 4;
 const lineNum = 5;
 const startNode = 1000;
+let stack = 0;
 let arr = [];
-const obj = {
-  999: [1000],
-  1000: [999],
-};
+const obj = [
+  [1, 0, 1, 1, 1, 1],
+  [1, 0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1, 1],
+  [1, 1, 1, 0, 1, 1],
+];
 
-
-const dfs = (startNode, obj) => {
-  arr.push(startNode);
-
-  for (let node of obj[startNode]) {
-    if (!arr.includes(node)) {
-      dfs(node, obj);
+const dfs = (obj, n, m) => {
+  let temp = false;
+  console.log(n,m)
+  if (obj[n + 1][m] === 1 && n !== obj.length - 1) {
+    const temp2 = dfs(obj, n + 1, m);
+    stack += 1;
+    temp = true;
+    if (temp2 === 0) {
+      stack -= 1;
     }
+  }
+  if (obj[n][m + 1] === 1 && m !== obj[0].length - 1) {
+    const temp2 = dfs(obj, n, m + 1);
+
+    stack += 1;
+    temp = true;
+    if (temp2 === 0) {
+      stack -= 1;
+    }
+  }
+  if (obj[n - 1][m] === 1 && n !== 0) {
+    const temp2 = dfs(obj, n - 1, m);
+    stack += 1;
+
+    temp = true;
+    if (temp2 === 0) {
+      stack -= 1;
+    }
+  }
+  if (obj[n][m - 1] === 1 && m !== 0) {
+    const temp2 = dfs(obj, m - 1);
+    stack += 1;
+
+    temp = true;
+    if (temp2 === 0) {
+      stack -= 1;
+    }
+  }
+
+  if (n === obj.length - 1 && m === obj[0].length - 1) {
+    stack += 1;
+    temp = true;
+  }
+
+  if (temp) {
+    return 1;
+  } else {
+    return 0;
   }
 };
 
-dfs(startNode, obj, arr);
-console.log(arr);
+dfs(obj, 0, 0);
+console.log(stack);
